cb15d6e1f24ef01f5898d82f8675b7101159a75e
Dmscndraa <72033896+Dimascndraa@users.noreply.github.com>
2022-10-18 21:18:28 +0700

    Add files via upload

---
+++ rsia-livasya/layout/header.php
@@ -0,0 +1,17 @@
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> medcare. </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#services">services</a>
+        <a href="#about">about</a>
+        <a href="#doctors">doctors</a>
+        <a href="#book">book</a>
+        <a href="#review">review</a>
+        <a href="#blogs">blogs</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
